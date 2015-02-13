a4d8874144809b7c217e949ca1b7b80da1a2eb53
Gosh <osvaldocube@gmail.com>
2015-02-13 04:07:00 -0600

    Initial Commit

---
+++ tragomaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tragomaps
+//
+//  Created by TRON on 13/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
