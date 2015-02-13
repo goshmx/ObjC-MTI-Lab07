abdd314b2cafd4334632f3b63f77262f2b912435
Gosh <osvaldocube@gmail.com>
2015-02-13 14:32:17 -0600

    Configurado el envio de notificaciones.

---
+++ tragomaps/Home.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tragomaps
+//
+//  Created by TRON on 13/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Home : UIViewController
+
+
+@end
+
---
+++ tragomaps/Home.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tragomaps
+//
+//  Created by TRON on 13/02/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import "Home.h"
+
+@interface Home ()
+
+@end
+
+@implementation Home
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
